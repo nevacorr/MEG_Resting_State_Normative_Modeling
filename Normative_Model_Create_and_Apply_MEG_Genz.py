2709223e939964553d8968491be127ee32a71e39
Neva <nevao@uw.edu>
2024-08-23 14:16:15 -0700

    Complete. All updated for new data set.

--- Normative_Model_Create_and_Apply_MEG_Genz.py
+++ Normative_Model_Create_and_Apply_MEG_Genz.py
@@ -11,7 +11,7 @@ from plot_and_compute_zdistributions import plot_and_compute_zcores_by_gender
 
 struct_var = 'meg'
 show_plots = 0          #set to 1 to show training and test data spline fit plots.
-show_nsubject_plots = 1 #set to 1 to plot number of subjects used in analysis, for each age and gender
+show_nsubject_plots = 0 #set to 1 to plot number of subjects used in analysis, for each age and gender
 spline_order = 1        # order of spline to use for model
 spline_knots = 2        # number of knots in spline to use in model
 perform_train_test_split_precovid = 0  # flag indicating whether to split training set (pre-covid data) into train and
