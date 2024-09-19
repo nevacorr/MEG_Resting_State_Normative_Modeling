c5ec92bf942f722e403ced80b7bb21bd1efdc491
Neva <nevao@uw.edu>
2024-09-19 13:09:57 -0700

    finished green/yellow custom colormap

--- demo_linear_segmented_colormaps.py
+++ demo_linear_segmented_colormaps.py
@@ -15,21 +15,27 @@ X = np.cos(A) * np.sin(B) * 10
 
 # custom segmented color dictionary
 
-cdict3 = {'red': ((0.0, 0.0, 0.0),
-                  (0.25, 0.0, 0.0),
-                  (0.5, 0.8, 1.0),
-                  (0.75, 1.0, 1.0),
-                  (1.0, 0.4, 1.0)),
+cdict3 = {'red': ((0.0, 1.0, 1.0), #bright yellow
+                  (0.25, 0.8, 0.8), #lighter yellow
+                  (0.49, 0.7, 0.7), #dark yellow
+                  (0.5, 0.5, 0.5),  #gray
+                  (0.51, 0.0, 0.0), #dark green
+                  (0.75, 0.0, 0.0), #green
+                  (1.0, 0.0, 0.0)), #bright green
 
-          'green': ((0.0, 0.0, 0.0),
-                    (0.25, 0.0, 0.0),
-                    (0.5, 0.9, 0.9),
-                    (0.75, 0.0, 0.0),
-                    (1.0, 0.0, 0.0)),
+          'green': ((0.0, 0.75, 1.0),
+                    (0.25, 0.8, 0.8),
+                    (0.49, 0.7, 0.7),
+                    (0.5, 0.5, 0.5),
+                    (0.51, 0.7, 0.7),
+                    (0.75, 0.8, 0.8),
+                    (1.0, 1.0, 1.0)),
 
-          'blue': ((0.0, 0.0, 0.4),
-                   (0.25, 1.0, 1.0),
-                   (0.5, 1.0, 0.8),
+          'blue': ((0.0, 0.0, 0.0),
+                   (0.25, 0.0, 0.0),
+                   (0.49, 0.0, 0.0),
+                   (0.5, 0.5, 0.5),
+                   (0.51, 0.0, 0.0),
                    (0.75, 0.0, 0.0),
                    (1.0, 0.0, 0.0))
           }
@@ -42,15 +48,15 @@ figure, axes = plt.subplots(figsize =(6, 9))
 
 # Making plot
 #----------------------------------------------------
-blue_red3 = LinearSegmentedColormap('BlueRed3', cdict3)
-plt.register_cmap(cmap=blue_red3)
-cmap = plt.get_cmap("BlueRed3")
+custom_colormap = LinearSegmentedColormap('CustomYellowGrayGreen', cdict3)
+plt.register_cmap(cmap=custom_colormap)
+# cmap = plt.get_cmap("BlueRed3")
 # set the third cmap as the default.
 # plt.rcParams['image.cmap'] = 'BlueRed3'
 
-img3 = plt.imshow(X, interpolation='nearest', cmap=cmap)
+img3 = plt.imshow(X, interpolation='nearest', cmap='CustomYellowGrayGreen')
 figure.colorbar(img3)
-plt.title("BlueRed3")
+plt.title("Custom Colormap")
 plt.show()
 #------------------------------------------------------
 
