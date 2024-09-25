3dbbe5e3168644578a5f3dc50a670e3252b3dd58
Neva <nevao@uw.edu>
2024-09-25 13:28:15 -0700

    Organize directories of plot and text file output

--- MakeFigureShowingRegionsWithSignificantPostCOVIDDeviations.py
+++ MakeFigureShowingRegionsWithSignificantPostCOVIDDeviations.py
@@ -7,10 +7,10 @@ from helper_functions_MEG import read_text_list
 
 working_dir = os.getcwd()
 
-regions_reject_female_file = f'{working_dir}/regions_reject_null_rsMEG_female.csv'
+regions_reject_female_file = f'{working_dir}/output_data/regions_reject_null_rsMEG_female.csv'
 regions_female = read_text_list(regions_reject_female_file)
 
-regions_reject_male_file = f'{working_dir}/regions_reject_null_rsMEG_male.csv'
+regions_reject_male_file = f'{working_dir}/output_data/regions_reject_null_rsMEG_male.csv'
 regions_male = read_text_list(regions_reject_male_file)
 
 rfemale = {}
