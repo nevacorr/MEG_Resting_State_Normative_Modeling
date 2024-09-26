091bdf290851caac3f5f9d6c60116bec4b32ace5
Neva <nevao@uw.edu>
2024-09-26 15:44:50 -0700

    minmaxscalar

--- Normative_Model_Create_and_Apply_MEG_Genz.py
+++ Normative_Model_Create_and_Apply_MEG_Genz.py
@@ -11,7 +11,7 @@ from plot_and_compute_zdistributions import plot_and_compute_zcores_by_gender
 from make_time1_normative_model_bootstrap import make_time1_normative_model_bootstrap
 
 struct_var = 'meg'
-show_plots = 1          #set to 1 to show training and test data spline fit plots.
+show_plots = 0          #set to 1 to show training and test data spline fit plots.
 show_nsubject_plots = 0 #set to 1 to plot number of subjects used in analysis, for each age and gender
 spline_order = 1        # order of spline to use for model
 spline_knots = 2        # number of knots in spline to use in model
