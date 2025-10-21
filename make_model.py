3b9d64eb8a7a6298d795672b3a4cc51465a0448f
Neva <nevao@uw.edu>
2025-10-21 14:37:45 -0700

    comment output plotting to make program run faster

--- make_model.py
+++ make_model.py
@@ -149,8 +149,8 @@ def make_model(rsd_v1_orig, rsd_v2_orig, struct_var, n_splits, train_set_array, 
                 model_slope.loc[band, roi], ymin.loc[band, roi] = calc_model_slope(dummy_cov_file_path, nm)
 
                 # compute splines and superimpose on data. Show on screen or save to file depending on show_plots value.
-                plot_data_with_spline_one_gender(sex, 'Training Data', band, cov_file_tr, resp_file_tr, dummy_cov_file_path,
-                                      model_dir, roi, show_plots, working_dir, dirdata)
+                # plot_data_with_spline_one_gender(sex, 'Training Data', band, cov_file_tr, resp_file_tr, dummy_cov_file_path,
+                #                       model_dir, roi, show_plots, working_dir, dirdata)
 
             # Z_time2[band] = apply_normative_model_time2(struct_var, show_plots, show_nsubject_plots, spline_order, spline_knots,
             #                     working_dir,rsd_v2, roi_ids, dirdata, dirpredict, sex, band)
@@ -162,7 +162,7 @@ def make_model(rsd_v1_orig, rsd_v2_orig, struct_var, n_splits, train_set_array, 
             # for band, df in Z2_all_splits_dict.items():
             #     df.to_csv(os.path.join(working_dir, 'output_data', f'Z2_allsplits_{band}_{n_splits}splits.csv'), index=False)
 
-        Z2_all_splits_dict[band] = {}
+            Z2_all_splits_dict[band] = {}
 
         # Save model slopes and ymins to file
         model_slope['split'] = split
