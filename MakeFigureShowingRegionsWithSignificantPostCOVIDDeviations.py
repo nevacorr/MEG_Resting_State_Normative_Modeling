cef77ef3bfda50b9495b9c3da70cfdb45c163fea
Neva <nevao@uw.edu>
2026-01-15 15:44:43 -0800

    exclude regions that don't show significant change with age from figures

--- MakeFigureShowingRegionsWithSignificantPostCOVIDDeviations.py
+++ MakeFigureShowingRegionsWithSignificantPostCOVIDDeviations.py
@@ -10,6 +10,20 @@ from helper_functions_MEG import read_text_list
 working_dir = os.getcwd()
 save_dir = working_dir + '/plots'
 
+bands = ['theta','alpha', 'beta', 'gamma']
+sexes = ['female', 'male']
+
+regions_sig_change_with_age = {}
+
+for sex in sexes:
+    regions_sig_change_with_age[sex] = {}
+    for band in bands:
+        path = os.path.join(working_dir, "output_data_bak", f"{sex}_regions_showing_significant_change_with_age_precovid_{band}_band.txt")
+        df = pd.read_csv(path, sep=r"\s+", header=None)
+        # Take only the first column (region names)
+        region_list = df.iloc[:, 0].tolist()
+        regions_sig_change_with_age[sex][band] = region_list
+
 Z2={}
 
 with open(os.path.join(working_dir, f'Zscores_post_covid_test_all_bands_female_100_splits.pkl'), 'rb') as f:
@@ -39,11 +53,34 @@ regions_female = read_text_list(regions_reject_female_file)
 regions_reject_male_file = f'{working_dir}/output_data/regions_reject_null_rsMEG_male.csv'
 regions_male = read_text_list(regions_reject_male_file)
 
+filtered = {}
+
+for sex in ['female', 'male']:
+    original_list = regions_female if sex == 'female' else regions_male
+    new_list = []
+
+    for item in original_list:
+        # Split into "region-hemi" and "band"
+        region_with_hemi, band = item.rsplit("_", 1)  # e.g., "precuneus-lh", "theta"
+
+        # Map hemisphere to match dictionary
+        if region_with_hemi.endswith("-lh"):
+            region_for_match = region_with_hemi[:-3] + "_left"   # remove "-lh", add "_left"
+        elif region_with_hemi.endswith("-rh"):
+            region_for_match = region_with_hemi[:-3] + "_right"  # remove "-rh", add "_right"
+
+        # Check if region is in significant-change list for that band
+        if region_for_match in regions_sig_change_with_age[sex][band]:
+            new_list.append(item)  # keep original format
+
+    filtered[sex] = new_list
+
+regions_female = filtered['female']
+regions_male   = filtered['male']
+
 r = {}
 female_dict = {}
 male_dict = {}
-
-sexes = ['female', 'male']
 
 # Define the colors for each integer value
 colors = ['steelblue', 'yellow']
@@ -56,7 +93,7 @@ for sex in sexes:
     elif sex == 'male':
         regions_list = regions_male
         mydict = male_dict
-    for i, band in enumerate(['theta','alpha', 'beta', 'gamma']):
+    for i, band in enumerate(bands):
         r[band] = [item for item in regions_list if band in item]
 
         mydict[band] = {}
@@ -75,7 +112,7 @@ for sex in sexes:
 
         dict_to_plot = mydict[band].copy()
 
-        filename = f'{sex.capitalize()} Regions with significantly altered power in post-COVID rsMEG {band} band'
+        filename = f'{sex.capitalize()} Regions with significantly altered power in post-COVID rsMEG {band} band_sig_change_with_age_only'
         myggseg.plot_dk(dict_to_plot, save_dir, filename, cmap=cmap, background='k', vminmax=[0, 3], edgecolor='w', bordercolor='gray', figsize=(8,8),
                           title=f'{sex.capitalize()} Regions with Significantly Increased Power in\nPost-COVID rsMEG {band.capitalize()} Band')
 
